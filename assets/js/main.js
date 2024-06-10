6849e9459ef702ecb2b2b4a2d6edf25c522a9ee8
Brotherbond <ehinmitankehinde@gmail.com>
2024-06-10 21:07:34 +0100

    10/06 testing

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+console.log("Hello Ayo");
+
+welcome.innerHTML = "Welcome here now now";
+welcome.onclick = () => console.error('Hello World, Welocdlf')
+
+
+
+
+
+const x = 32, y = 11;
+let z = 6;
+// let x = 78;
+
+// x = 34;
+
+
+console.log("Hello Ayo", x, z, y);
+
+function foodie(x) {
+    console.log(x);
+};
+
+// const foodie = () => {  // arrow function anonymous
+//     console.log(x);
+// }
+
+foodie(z); 
+
+
+
+
+number_of_donuts = 34
+
+const medicaldoctors = 23
+
+const stepHenHouseRoad = "hello world"
+const StepHenHouseRoad = "hello world"
+const step_hen_house_road = "hello world"
+// const step-hen-house-road = "hello world"
+const stephen = "hello world"
+
+
+
